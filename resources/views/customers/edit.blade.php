79950bddb4138729396c9bfbca0bf563e1391d14
yeisharoberts <62228994+yeisharoberts@users.noreply.github.com>
2022-01-24 18:20:22 +0800

    Update edit.blade.php

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -51,7 +51,7 @@
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline inline-block">
                       Update
                     </button>
-                    <a href="/peoples" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline inline-block">
+                    <a href="/customers" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline inline-block">
                         Cancel
                     </a>
                   </div>
